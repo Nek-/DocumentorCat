52eb37055c2cc6868c3c17af4468514a29b6f30b
Nek <nek.dev@gmail.com>
2013-01-07 01:42:52 +0100

    Added main file.

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <regex.h>
+
+/**
+ * \file      main.c
+ * \author    HappyTeam
+ * \version   1.0
+ * \date      7/01/2012
+ * \brief     Fonctionnement principal
+ *
+ */
+
+int main()
+{
+    printf("Hello !");
+    return 0;
+}
